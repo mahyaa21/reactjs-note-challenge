f201111d806c3ac6235935d817b79206e7c864ba
mahya <m.kabiri@shab.ir>
2020-11-06 12:11:11 +0330

    UPDATE: add redux tools into store

--- reactjs-note-challenge/src/store.js
+++ reactjs-note-challenge/src/store.js
@@ -1,17 +1,29 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './redux/Sagas';
 import rootReducer from './redux/Reducers';
 const initialState = {
-
+    note: {},
+    user: {}
 }
 
 const sagaMiddleware = createSagaMiddleware()
+const composeEnhancers =
+    typeof window === 'object' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
 
+const enhancer = composeEnhancers(
+    applyMiddleware(sagaMiddleware),
+    // other store enhancers if any
+);
 export const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(sagaMiddleware)
+    enhancer,
+    // applyMiddleware(sagaMiddleware)
 )
 sagaMiddleware.run(rootSaga)
 
